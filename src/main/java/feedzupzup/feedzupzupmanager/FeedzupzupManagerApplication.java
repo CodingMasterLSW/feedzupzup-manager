ce473836f51e474b49c00ac5a9aa06f5bf34198f
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-12-09 22:22:10 +0900

    chore: init project

---
+++ src/main/java/feedzupzup/feedzupzupmanager/FeedzupzupManagerApplication.java
@@ -0,0 +1,13 @@
+package feedzupzup.feedzupzupmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedzupzupManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedzupzupManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/feedzupzup/feedzupzupmanager/FeedzupzupManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package feedzupzup.feedzupzupmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedzupzupManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
